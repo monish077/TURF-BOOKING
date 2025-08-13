8edbeb29be421c307e6563395ef08d1ee877c3f7
Monish M <monidhoni0007@gmail.com>
2025-08-13 11:19:03 +0530

    style is basic

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import AppRouter from "./router/AppRouter";
+import "./App.css"; // ✅ Import global styles
 
 function App() {
   return (
-    <>
+    <div className="app-container">
       <AppRouter />
-    </>
+    </div>
   );
 }
 
